1af00a8a623f194ec47541c97c8c11ffd2a6feab
vvh413 <nnenekit@gmail.com>
2022-10-21 20:57:24 +0300

    isolate prover and verifier in 2 threads

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,19 @@ async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let t: usize = args.get(1).expect("expects param `t`").parse()?;
 
-    let peggy = Prover::new(t);
-    let victor = Verifier::new(t);
-
     let (tx1, rx1): (Sender<u64>, Receiver<u64>) = channel(1);
     let (tx2, rx2): (Sender<u64>, Receiver<u64>) = channel(1);
 
-    tokio::try_join!(peggy.run(tx1, rx2), victor.run(tx2, rx1))?;
+    tokio::try_join!(
+        tokio::spawn(async move {
+            let peggy = Prover::new(t);
+            peggy.run(tx1, rx2).await.unwrap()
+        }),
+        tokio::spawn(async move {
+            let victor = Verifier::new(t);
+            victor.run(tx2, rx1).await.unwrap()
+        })
+    )?;
 
     Ok(())
 }
